cf4d89275e0aa93b8e0de07025eee4f7ef506aa6
Alan Vazquez <50334817+AlanIsaacV@users.noreply.github.com>
2024-05-07 03:20:53 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var members = [
+    { name: 'John Doe', skills: ['HTML', 'CSS', 'JS'] },
+    { name: 'Jane Doe', skills: ['HTML', 'CSS', 'JS'] },
+    { name: 'Jim Doe', skills: ['HTML', 'CSS', 'JS'] }
+  ];
+  return members;
+}
